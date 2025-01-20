4117bec9aa7526294d3e806d3e4964ba6ee3aa01
maxxxkam <muslimovmaxkam@gmail.com>
2025-01-20 18:21:44 +0500

    Add files via upload

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect,useRef } from 'react';
-import s from './Catalog.module.scss';
+import React, { useEffect, useRef } from "react";
+import s from "./Catalog.module.scss";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/scrollbar';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { Keyboard, Navigation, Scrollbar, Pagination } from 'swiper/modules';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Keyboard, Navigation, Scrollbar, Pagination } from "swiper/modules";
 
 const Catalog = () => {
   useEffect(() => {
@@ -25,73 +24,95 @@ const Catalog = () => {
     <>
       <section className={s.catalog}>
         <div className="container">
-          <div className={s.wrapper}>
-          <Swiper
-  slidesPerView={2} 
-  slidesPerGroup={2}
-  loop={true} 
-  grabCursor={true}
+        <Swiper
+  spaceBetween={60}
+  slidesPerView={1}
+  slidesPerGroup={1}
+  loop={true}
+  grabCursor={false}
   keyboard={{
     enabled: true,
   }}
   breakpoints={{
-    769: {
+    // Для ширины экрана меньше или равной 800px
+   
+    // Для ширины экрана больше 800px
+    801: {
       slidesPerView: 2,
       slidesPerGroup: 2,
     },
   }}
-  scrollbar={true}
-  navigation={true} 
+  scrollbar={false}
+  navigation={true}
   pagination={{
     clickable: true,
   }}
   modules={[Keyboard, Scrollbar, Navigation, Pagination]}
   className={s.mySwiper}
 >
-  <SwiperSlide>
-    <div className={`${s.card} ${s.card_1}`} data-aos="fade-down" data-aos-delay="200">
-      <h1> Get unblocked with a little help from AI </h1>
-      <img src="/chat-img.png" alt="" />
-      <p>
-        Brainstorm ideas, create action plans, generate to-do lists
-        and more. Organize your thoughts and keep your projects
-        moving forward with Daisy.
-      </p>
-    </div>
-  </SwiperSlide>
-  <SwiperSlide>
-    <div className={`${s.card} ${s.card_2}`} data-aos="fade-down" data-aos-delay="400">
-      <h1>Save inspiration anytime, anywhere</h1>
-      <img src="/catalog-img.png" alt="" />
-      <p>
-        Access all your inspiration in one place. Curate your
-        personal creative library — images, videos, websites, and
-        GIFS, at your fingertips. Drag and drop elements onto your
-        canvas and watch your vision unfold.
-      </p>
-    </div>
-  </SwiperSlide>
-  <SwiperSlide>
-    <div className={`${s.card} ${s.card_3}`} data-aos="fade-down" data-aos-delay="600">
-      <h1>Achieve More Together with AI</h1>
-      <img src="/catalog-img3.jpg" alt="" />
-      <p>
-      Collaborate seamlessly with your team to develop groundbreaking AI solutions. Share ideas, refine strategies, and turn innovation into impactful results.
-      </p>
-    </div>
-  </SwiperSlide>
-  <SwiperSlide>
-    <div className={`${s.card} ${s.card_4}`} data-aos="fade-down" data-aos-delay="800">
-      <h1>Shaping the Future of Technology</h1>
-      <img src="/catalog-gif.gif" alt="" />
-      <p>
-      Explore the limitless potential of abstract technologies. From dynamic designs to futuristic concepts, unleash creativity and push boundaries into the unknown.
-      </p>
-    </div>
-  </SwiperSlide>
-</Swiper>
 
-          </div>
+            <SwiperSlide>
+              <div
+                className={`${s.card} ${s.card_1}`}
+                data-aos="fade-down"
+                data-aos-delay="200"
+              >
+                <h1> Get unblocked with a little help from AI </h1>
+                <img src="/chat-img.png" alt="" />
+                <p>
+                  Brainstorm ideas, create action plans, generate to-do lists
+                  and more. Organize your thoughts and keep your projects moving
+                  forward with Daisy.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+                className={`${s.card} ${s.card_2}`}
+                data-aos="fade-down"
+                data-aos-delay="400"
+              >
+                <h1>Save inspiration anytime, anywhere</h1>
+                <img src="/catalog-img.png" alt="" />
+                <p>
+                  Access all your inspiration in one place. Curate your personal
+                  creative library — images, videos, websites, and GIFS, at your
+                  fingertips. Drag and drop elements onto your canvas and watch
+                  your vision unfold.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+                className={`${s.card} ${s.card_3}`}
+                data-aos="fade-down"
+                data-aos-delay="600"
+              >
+                <h1>Achieve More Together with AI</h1>
+                <img src="/catalog-img3.jpg" alt="" />
+                <p>
+                  Collaborate seamlessly with your team to develop
+                  groundbreaking AI solutions. Share ideas, refine strategies,
+                  and turn innovation into impactful results.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+                className={`${s.card} ${s.card_4}`}
+                data-aos="fade-down"
+                data-aos-delay="800"
+              >
+                <h1>Shaping the Future of Technology</h1>
+                <img src="/catalog-gif.gif" alt="" />
+                <p>
+                  Explore the limitless potential of abstract technologies. From
+                  dynamic designs to futuristic concepts, unleash creativity and
+                  push boundaries into the unknown.
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </section>
     </>
